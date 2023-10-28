91130c1390610255c34214d854c92945b5e6f6c9
dvmduc5269 <ducduvumanh@gmail.com>
2023-10-28 21:56:35 +0700

    add addtional code

--- Lab01/ChoosingOption.java
+++ Lab01/ChoosingOption.java
@@ -7,6 +7,14 @@ public class ChoosingOption {
                 "Do you want to change to the first class ticket?");
         JOptionPane.showMessageDialog(null,
                 "You have chosen: " + (option == JOptionPane.YES_OPTION?"Yes":"No"));
+
+        String[] options = {"Yes", "No"};
+        option = JOptionPane.showOptionDialog(null, "Do you want to continue?",
+                "Confirmation",
+                JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
+
+        JOptionPane.showMessageDialog(null,
+                "You have chosen: " + (option == JOptionPane.YES_OPTION?"Yes":"No"));
         System.exit(0);
     }
 
